c0300732e5759dee86260f89cb89cd31bb964226
Grm Shalini <88344466+ShaliniGudipati@users.noreply.github.com>
2022-12-08 13:06:25 +0530

    Update index.js
    shalini

--- server/index.js
+++ server/index.js
@@ -10,6 +10,12 @@ const saltRounds = 10;
 
 const app = express();
 
+import express from "express"
+import mysql from "mysql"
+import cors from "cors"
+
+const app = express()
+
 app.use(express.json());
 app.use(
   cors({
@@ -95,6 +101,68 @@ app.post("/login", (req, res) => {
     }
   );
 });
+
+
+
+const db = mysql.createConnection({
+    host:"localhost",
+    user:"sunny",
+    password:"Omsairam@123",
+    database:"test"
+}); 
+
+app.get("/", (req,res)=>{
+    res.json("hello this is backend")
+});
+
+app.use(express.json())
+app.use(cors())
+
+app.get("/books",(req,res)=>{
+    const q="select * from books"
+    db.query(q,(err,data)=>{
+        if(err) return res.json(err)
+        return res.json(data)
+    })
+})
+
+app.post("/books",(req,res)=>{
+    const q="insert into books (`title`,`desc`,`cover`) Values(?)"
+    //const values=["title from backend","desc from backend","cover pic"];
+    const values=[
+        req.body.title,
+        req.body.desc,
+        req.body.cover
+    ];
+    db.query(q,[values],(err,data)=>{
+        if(err) return res.json(err)
+        return res.json("Books have been inserted ")
+    })
+})
+
+app.put("/books/:id",(req,res)=>{
+    const bookId=req.params.id;
+    const q="update books set title=?, desc=?, price=?, cover=? where id=?"
+    const values=[
+        req.body.title,
+        req.body.desc,
+        req.body.cover,
+        req.body.price
+    ];
+    db.query(q,[...values,bookId] ,(err,data)=>{
+        if(err) return res.json(err)
+        return res.json("Updated")
+    })
+})
+
+app.delete("/books/:id",(req,res)=>{
+    const bookId=req.params.id;
+    const q="delete from books where id=?"
+    db.query(q,bookId,(err,data)=>{
+        if(err) return res.json(err)
+        return res.json("delete")
+    })
+})
 
 app.listen(8800, () => {
   console.log("running server");
