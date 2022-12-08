9129a30846e593fc0ca83df4a9daafed32154de9
Grm Shalini <88344466+ShaliniGudipati@users.noreply.github.com>
2022-12-08 13:09:26 +0530

    Update App.js
    shalini

--- client/src/App.js
+++ client/src/App.js
@@ -4,14 +4,30 @@ import "./App.css";
 import Main from "./pages/Main";
 import Login from "./pages/Login";
 import Registration from "./pages/Registration";
+import './App.css';
+import { BrowserRouter, Routes, Route} from "react-router-dom"
+import Books from './pages/Books';
+import Add from './pages/Add';
+import Update from './pages/Update';
+import "./style.css"
+
+
+export default App;
+
 
 function App() {
   return (
-    <Router>
-      <Route path="/registration" exact render={(props) => <Registration />} />
+ 
+    <BrowserRouter>
+        <Routes>
+        <Route path="/registration" exact render={(props) => <Registration />} />
       <Route path="/login" exact render={(props) => <Login />} />
       <Route path="/" exact render={(props) => <Main />} />
-    </Router>
+          <Route path="/books" element={<Books></Books>}/>
+          <Route path="/add" element={<Add></Add>}/>
+          <Route path="/update/:id" element={<Update></Update>}/>
+        </Routes>
+      </BrowserRouter>
   );
 }
 
